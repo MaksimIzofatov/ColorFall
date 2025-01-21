e7937a9922109bc66dd3af186e439cae42343265
Максим Изофатов <giantsgnom@gmail.com>
2025-01-21 10:19:44 +0300

    add color change

--- Assets/Scripts/plane.cs
+++ Assets/Scripts/plane.cs
@@ -2,18 +2,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class plane : MonoBehaviour
 {
     [SerializeField] private Grid _grid;
     [SerializeField] private GameObject _prefabCube;
     [SerializeField] private float _spawnTime = 0.04f;
+    [SerializeField] private int _countCubes = 20;
+    
+    [SerializeField] private float _timeToChangeColor = 0.5f;
+    [SerializeField] private float _durationChange = 0.2f;
 
-    private List<CubeColor> _cubes;
+    private CubeColor[,] _cubes;
 
     private void Start()
     {
-        _cubes = new();
+        _cubes = new CubeColor[_countCubes, _countCubes];
         StartCoroutine(SpawnCubes());
     }
 
@@ -24,10 +29,9 @@ public class plane : MonoBehaviour
             for (int z = 0; z < 20; z++)
             {
                 var cellWorldPos = _grid.GetCellCenterWorld(new Vector3Int(z-11, 0, x-11));
-                //var cube = Instantiate(_prefabCube, cellWorldPos, Quaternion.identity);
                 var cube = Instantiate(_prefabCube);
                 cube.transform.position = cellWorldPos + new Vector3(0, 0.15f);
-                _cubes.Add(cube.GetComponent<CubeColor>());
+                _cubes[z, x] = cube.GetComponent<CubeColor>();
                 yield return new WaitForSeconds(_spawnTime);
             }
         }
@@ -35,35 +39,23 @@ public class plane : MonoBehaviour
 
     public void ChangeColor()
     {
-        
+        StartCoroutine(ChangeColorCoroutine());
     }
 
     private IEnumerator ChangeColorCoroutine()
     {
-        yield break;
-    }
+        var newColor = Random.ColorHSV();
 
-    private void OnDrawGizmos()
-    {
-        var cellSize = _grid.cellSize;
-        var cellGap = _grid.cellGap;
-        var origin = _grid.transform.position +new Vector3(-11f,0,-11f);
-
-        Gizmos.color = Color.red;
-
-        for (int x = 0; x < 20; x++)
+        for (int x = 19; x>= 0; x--)
         {
             for (int z = 0; z < 20; z++)
             {
-                var pos = origin + new Vector3(x * (cellSize.x + cellGap.x), 0, z * (cellSize.z + cellGap.z));
-
-                Gizmos.DrawLine(pos, pos + new Vector3(cellSize.x, 0, 0));
-                Gizmos.DrawLine(pos, pos + new Vector3(0, 0, cellSize.z));
-                Gizmos.DrawLine(pos + new Vector3(cellSize.x, 0, 0), pos + new Vector3(cellSize.x, 0, cellSize.z));
-                Gizmos.DrawLine(pos + new Vector3(0, 0, cellSize.z), pos + new Vector3(cellSize.x, 0, cellSize.z));
-
+                _cubes[z, x].ChangeColor(newColor, _timeToChangeColor);
+                yield return new WaitForSeconds(_durationChange);
             }
         }
     }
+
+    
 
 }
